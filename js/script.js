899b9ba793e3df048dd99659df55e6d93ea3ebbe
Marcos de Lima Prado <126472671+marcoslprado@users.noreply.github.com>
2024-09-13 15:32:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,85 @@
-navigator.geolocation.getCurrentPosition((position)=> {
-    console.log(position);
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-});
+// TO-DO:
+// Organizar código-fonte,
+
 
 const weekDay = document.getElementById("week-day");
 const currentDate = document.getElementById("date");
 const currentHour = document.getElementById("current-hour");
-const registerCheckpointButton = document.getElementById("register-checkpoint-button");
 
+
+const registerCheckpointButton = document.getElementById("register-checkpoint-button");
 registerCheckpointButton.addEventListener("click", register);
 
 const dialogCheckpoint = document.getElementById("dialog-checkpoint");
 
-const closeButton = document.getElementById("dialog-close")
+const closeButton = document.getElementById("dialog-close");
 closeButton.addEventListener("click", () => {
     dialogCheckpoint.close();
 })
 
-const dialogDate = document.getElementById("dialog-date");
-dialogDate.textContent = getCurrentDate();
+let registerLocalStorage = getRegisterLocalStorage();
 
+const dialogDate = document.getElementById("dialog-date");
 const dialogHour = document.getElementById("dialog-hour");
+
+dialogDate.textContent = getCurrentDate();
 dialogHour.textContent = getCurrentTime(); 
 
-const btnDialogIn = document.getElementById("btn-dialog-in");
-btnDialogIn.addEventListener("click", () => {
-    // recuperar informações
-    // data, hora, localização (lat,long), tipo: entrada
-    // salvar essas informações num objeto Javascript
-})
-
-function register() {
-    dialogCheckpoint.showModal();
+function getCurrentPosition() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        return position;
+    });
 }
 
-// Imprime a Data e horário no HTML
-function updateContentHour() {
-    currentDate.textContent = getCurrentDate();
-    currentHour.textContent = getCurrentTime();
-    weekDay.textContent = getWeekDay();
+const btnDialogIn = document.getElementById("btn-dialog-register-checkpoint");
+btnDialogIn.addEventListener("click", () => {
+    let typeRegister = document.getElementById("checkpoint-types");
+
+    let checkpoint = {
+        "date": getCurrentDate(),
+        "hour": getCurrentTime(),
+        "location": getCurrentPosition(),
+        "id": 1,
+        "type": typeRegister
+    }
+
+    console.log(checkpoint);
+
+    saveRegisterLocalStorage(checkpoint);
+
+    localStorage.setItem("lastTypeRegister", typeRegister);
+
+    dialogCheckpoint.close();
+
+    // TO-DO:
+    // Fechar o dialog ao bater ponto e apresentar, de alguma forma
+    // uma confirmação (ou não) para o usuário
+})
+
+
+function saveRegisterLocalStorage(register) {
+    registerLocalStorage.push(register); // Array
+    localStorage.setItem("register", JSON.stringify(registerLocalStorage));
+} 
+
+// Esta função deve retornar sempre um ARRAY, mesmo que seja vazio
+function getRegisterLocalStorage() {
+    let registers = localStorage.getItem("register");
+
+    if(!registers) {
+        return [];
+    }
+
+    return JSON.parse(registers); // converte de JSON para Array
+}
+
+
+function register() {
+    // TO-DO:
+    // Atualizar hora a cada segundo e data 00:00:00
+    dialogDate.textContent = "Date: " + getCurrentDate();
+    dialogHour.textContent = "Hour: " + getCurrentTime();
+    dialogCheckpoint.showModal();
 }
 
 // Retorna a hora atual(hora/minuto/segundo)
@@ -53,9 +93,21 @@ function getCurrentTime() {
 
 // Retorna a data atual no padrão dd/mm/aaaa
 function getCurrentDate() {
+    // TO-DO:
+    // Alterar a solução para considerar padStart ou slice
+    // Considerar formatos diferentes da data, conforme localização
+    // do usuário dd/mm/aaaa, mm/dd/aaaa, aaaa/mm/dd, aaaa.mm.dd
+    // Verificar se no Date() há algum método que possa auxiliar
+    // locale
     const date = new Date();
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
+    let month = date.getMonth();
+    let day = date.getDate();
+    if (day < 10) {
+        day = "0" + day
+    }
+    if (month < 10) {
+        month = "0" + (month + 1)
+    }
     return day + "/" + month + "/" + date.getFullYear();
 }
 
@@ -67,7 +119,9 @@ function getWeekDay() {
     return daynames[day];
 }
 
-updateContentHour();
-setInterval(updateContentHour, 1000); // Faz algo ser executado a cada 1000 milisegundos, nesse caso a função de exibir a data/hora
+function printCurrentHour() {
+    currentHour.textContent = getCurrentHour();
+}
 
-//Ver dialog(HTML)
+printCurrentHour();
+setInterval(printCurrentHour, 1000); // Faz algo ser executado a cada 1000 milisegundos, nesse caso a função de exibir a data/hora
